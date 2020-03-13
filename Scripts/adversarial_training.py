3bfae23ba026734b43d29305f24b6fbf9654fac9
RaiBP <raimundobecerraparra@gmail.com>
2020-03-13 00:23:51 -0300

    Adversarial training script
    Signed-off-by: RaiBP <raimundobecerraparra@gmail.com>

--- Scripts/adversarial_training.py
+++ Scripts/adversarial_training.py
@@ -1,21 +1,23 @@
 import numpy as np
 import tensorflow as tf
 import random
-from imagenetData import ImageNetData
+from imagenetData import ImageNetData, labels_to_one_hot
 from tensorflow.python.keras.models import load_model
 import models_and_utils as mm
+from tensorflow.python.keras.utils import to_categorical
 
-model = load_model('best_model.hdf5')
 batch_size = 64
 epochs = 5
 images_per_class = 500
 batch_number = int(images_per_class / batch_size)
 dropout_rate = .2
-classes = [96, 950]  # ,447, 530, 592, 950, 96]
+classes = [96, 950, 530]  # ,447, 530, 592, 950, 96]
 n_classes = len(classes)
 imageNet = ImageNetData(classes, images_per_class=500,
                         batch_size=batch_size, validation_proportion=0.4, augment_data=True)
 
+model = mm.define_model(n_classes, own_num=1)
+model.load_weights('best_model_1.hdf5')
 
 def adversarial_pattern(image, label):
     image = tf.cast(image, tf.float32)
@@ -31,8 +33,24 @@ def adversarial_pattern(image, label):
 
     return signed_grad
 
+def adversarial_step_ll(image):
+    image = tf.cast(image, tf.float32)
 
-def generate_adversarials(number_of_examples):
+    with tf.GradientTape() as tape:
+        tape.watch(image)
+        prediction = model(image)
+        y_ll = model.predict(image.copy()[np.newaxis, :]).argmin()
+        y_ll = labels_to_one_hot([y_ll], n_classes)[0]
+        loss = tf.keras.losses.MSE(y_ll, prediction)
+
+    gradient = tape.gradient(loss, image)
+
+    signed_grad = -1 * tf.sign(gradient)
+
+    return signed_grad
+
+
+def generate_adversarials(number_of_examples, epsilon=None):
     while True:
         x = []
         original_x = []
@@ -51,7 +69,8 @@ def generate_adversarials(number_of_examples):
 
             perturbations = adversarial_pattern(image.reshape((1, 224, 224, 3)), label).numpy()
 
-            epsilon = tf.abs(tf.truncated_normal(mean=0, stddev=8))
+            if epsilon is None:
+                epsilon = tf.abs(tf.truncated_normal(mean=0, stddev=8))
 
             adversarial = image + perturbations * epsilon
 
@@ -66,9 +85,9 @@ def generate_adversarials(number_of_examples):
         yield x, original_x, y
 
 
-x_adversarial_test, x_original_test, y_adversarial_test = next(generate_adversarials(50))
 x_adversarial_train, x_original_train, y_adversarial_train = next(generate_adversarials(300))
 
+x_adversarial_test, x_original_test, y_adversarial_test = next(generate_adversarials(50))
 
 test_accuracy_before = mm.to_test_model(model, imageNet)
 adv_test_accu_before = model.evaluate(x=x_adversarial_test, y=y_adversarial_test, verbose=0)[1]
