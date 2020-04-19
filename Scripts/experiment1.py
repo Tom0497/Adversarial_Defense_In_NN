49f687e157b8bd26a7d08dcafb5b10ccfcdb3b2e
RaiBP <raimundobecerraparra@gmail.com>
2020-04-19 03:59:49 -0400

    modified experiment1 script
    Signed-off-by: RaiBP <raimundobecerraparra@gmail.com>

--- Scripts/experiment1.py
+++ Scripts/experiment1.py
@@ -5,7 +5,8 @@ import tensorflow as tf
 import sys
 import time
 
-from tensorflow.python.keras.applications.resnet50 import ResNet50, preprocess_input
+from DataExtractor import get_labels_for_wnid
+from tensorflow.python.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 from utils import image_getter
 
 tf.compat.v1.enable_eager_execution()
@@ -21,9 +22,7 @@ def adversarial_pattern(model, image, label):
 
     gradient = tape.gradient(loss, image)
 
-    signed_grad = tf.sign(gradient)
-
-    return signed_grad
+    return gradient
 
 
 def adversarial_step_ll(model, image, num_classes):
@@ -64,7 +63,7 @@ def labels_to_one_hot(original_labels, number_of_classes):
 if __name__ == "__main__":
     use_step_ll = False
     plot_resulting_examples = False
-    attack_type = 'fg'
+    attack_type = 'rfgs'
 
     model = ResNet50(weights='imagenet')
     model.compile(optimizer=tf.keras.optimizers.RMSprop(),
@@ -73,6 +72,9 @@ if __name__ == "__main__":
 
     inputs = sys.argv
     current_directory = os.getcwd()
+    directory_path = os.path.dirname(current_directory)
+    CLASSES_FILE = "imagenet_class_index.json"
+    DICT_FILE_PATH = os.path.join(directory_path, "image_metadata", CLASSES_FILE)
     images_path = os.path.dirname(current_directory) + "/images"
 
     folders_names = os.listdir(images_path)
@@ -81,8 +83,8 @@ if __name__ == "__main__":
     labels = []
     images = []
 
-    actual_num_classes = len(dirs)  # cambiar aquí para cambiar el número de clases a usar
-    images_per_class = 3            # cambiar aquí para cambiar el número de imágenes por clase a usar
+    actual_num_classes = 50  # cambiar aquí para cambiar el número de clases a usar
+    images_per_class = 1           # cambiar aquí para cambiar el número de imágenes por clase a usar
 
     dirs = dirs[:actual_num_classes]
 
@@ -96,7 +98,7 @@ if __name__ == "__main__":
     accuracy_list = []
     accuracy_5_list = []
 
-    epsilons = np.linspace(0, 10, 21)
+    epsilons = np.linspace(0, 10000, 6)
 
     for epsilon in epsilons:
         epsilon_og = epsilon
@@ -115,9 +117,11 @@ if __name__ == "__main__":
             assert (len(images) == images_per_class)
 
             for img in images:
+                img = preprocess_input(img.copy())
+
                 if epsilon == 0:
                     start = time.process_time()
-                    y_pred = model.predict(preprocess_input(img.copy())[np.newaxis, :])
+                    y_pred = model.predict(img.copy()[np.newaxis, :])
 
                 else:
                     start = time.process_time()
@@ -135,7 +139,6 @@ if __name__ == "__main__":
 
                         if attack_type == 'fg':
                             perturbations = (output_gradient.numpy() / np.linalg.norm(output_gradient.numpy()))
-                            epsilon *= (1.7 * img.shape[0])
                         else:
                             perturbations = tf.sign(output_gradient).numpy()
 
@@ -145,7 +148,7 @@ if __name__ == "__main__":
                     img_adv = img_adv[0]
                     difference = img_adv.copy() - img.copy()
 
-                    y_pred = model.predict(preprocess_input(img_adv.copy())[np.newaxis, :])
+                    y_pred = model.predict(img_adv.copy()[np.newaxis, :])
 
                     if plot_resulting_examples:
                         plt.figure()
@@ -166,7 +169,13 @@ if __name__ == "__main__":
 
                         plt.show()
 
-                pred, pred_5 = y_pred.argmax(), np.argpartition(np.squeeze(y_pred), -5)[-5:]
+                #pred, pred_5 = y_pred.argmax(), np.argpartition(np.squeeze(y_pred), -5)[-5:]
+                pred_raw = decode_predictions(y_pred, top=1)[0][0]
+                pred = int(get_labels_for_wnid(pred_raw[0], DICT_FILE_PATH)[0])
+                pred_5_raw = decode_predictions(y_pred, top=5)[0]
+                pred_5 = []
+                for top5 in pred_5_raw:
+                    pred_5.append(int(get_labels_for_wnid(top5[0], DICT_FILE_PATH)[0]))
 
                 print(
                     f'Image {img_idx} out of {len(dirs) * images_per_class} (eps = {epsilon_og}). Elapsed time: {time.process_time() - start}')
